d6be2715342c618bf39bfce40133bf6a7ed747fb
Vlad Ivanov <nozto2005@gmail.com>
2026-08-11 14:30:49 +0200

    Initial commit

---
+++ src/main/java/be/kdg/programming5/onepiece/OnePieceAppSpring5Application.java
@@ -0,0 +1,13 @@
+package be.kdg.programming5.onepiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePieceAppSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnePieceAppSpring5Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/kdg/programming5/onepiece/OnePieceAppSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.programming5.onepiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePieceAppSpring5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
